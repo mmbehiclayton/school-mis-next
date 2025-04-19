d235f4b98cfd31a5980037396b08645cca00c8ca
mmbehiclayton <mmbehiclayton@gmail.com>
2025-04-19 18:52:41 +0300

    add navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
+import Menu from "@/components/Menu";
 import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 
 export default function DashboardLayout({
@@ -9,16 +11,22 @@ export default function DashboardLayout({
   return (    
       <div className="h-screen flex">
         {/* Left Sidebar */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
           <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          {/* Add Logo */}
           <img src="/logo.png" alt="Logo" width={32} height={32} />
+          {/* Add App Name on Logo? */}
           <span className="hidden lg:block text-xl font-bold">Next School</span>
-
           </Link>
+          {/* Add Menu */}
+          <Menu/>
         </div>
 
         {/* Main Content Area */}
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">{children}</div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] p-4 bg-[F7F8FA] overflow-scroll">
+          <Navbar/>
+          {children}
+        </div>
       </div>   
   );
 }
